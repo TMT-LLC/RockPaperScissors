3425e68d0b90554772abcbad410b630b07b295d3
Richard Harris <245955565+TMT-LLC@users.noreply.github.com>
2025-12-16 19:31:27 -0500

    Clean up user choice selection logic/algorithm
    Made sure that the user's choice functions are
    focusing on solving one problem at a time.
    Thus, I separated getHumanSelection into two functions,
    one for input and localization (make lowecase) and another
    to validate and ensure that the input is a valid option.
    Furthermore, I was able to clean up the code substantially,
    improving efficiency and readability.
    
    Moving on to the computer choice selection function.

--- .js
+++ .js
@@ -6,21 +6,31 @@
 // the 'Computer' side. Output the 'Computer' choice and display user's choice.
 // Show who the winner is and keep track of all previous moves.
 
-// Input for user's choice
-
-function getHumanChoice(choice) {
-    choice = prompt("Rock, Paper, or Scissors? (Don't worry, the computer won't cheat...)")
-    // Ensures that user chooses Rock, Paper, or Scissor only.
-    choice = choice.toLowerCase()
-    if (choice === "rock" || choice === "paper" || choice === "scissors") {
-        // Output successful choice message, can move onto next function for CPU
-        console.log("Valid Option!")
-        return choice;
-    } else {
-        // Output unsuccessful choice message, ask user to try again
-        return console.log("Invalid Option, try again!");
+// User's choice selection logic/algorithm
+    // Input for user's choice
+    function getHumanChoice() {
+        // Asks for user's choice
+        // Ensures that user's input is always converted to lowercase for validation
+        let choice = prompt("Rock, Paper, or Scissors? (Don't worry, the computer won't cheat...)").toLowerCase();
+        // Validates choice once lowercased to match either rock, paper, scissors, or false if invalid
+        return validateHumanChoice(choice)
     }
-}
+
+    // Validate user's choice
+    function validateHumanChoice (choice) {
+        // Only allows choice to be following options
+        if ((choice === "rock") | ("paper") | ("scissors")) {
+            // Output successful choice message, can move onto next function for CPU
+            console.log("Valid Option!")
+            // Returns valid choice option
+            return choice;
+        } else {
+            // Output unsuccessful choice message, ask user to try again
+            console.log("Invalid Option, try again!");
+            // Returns false, unable to use value for getHumanChoice
+            return false
+        }
+    }
 
 // Random choice for the 'Computer'
 function getComputerChoice() {
@@ -55,25 +65,25 @@ function rps(humanSelection, computerSelection) {
     // User win scenario
     if ((humanSelection === "rock" && computerSelection === "scissors") || (humanSelection ==="paper" && computerSelection === "rock") || (humanSelection === "scissors" && computerSelection === "paper"
     )){
-        console.log("User chose: " + humanSelection  + ". Computer chose: " + computerSelection + ". User Wins!")
+        console.log("User chose: " + humanSelection  + "! Computer chose: " + computerSelection + ". User Wins!")
         console.log("User has won: " + (u + 1) + " time(s).")
-        return true
+        return u + 1
     } 
     // Computer win scenario
     else if ((computerSelection === "rock" && humanSelection === "scissors") || (computerSelection ==="paper" && humanSelection === "rock") || (computerSelection === "scissors" && humanSelection === "paper"
     )){
-        console.log("User chose: " + humanSelection  + ". Computer chose: " + computerSelection + ". Computer Wins!")
+        console.log("User chose: " + humanSelection  + "! Computer chose: " + computerSelection + ". Computer Wins!")
         console.log("Computer has won: " + (c + 1) + " time(s).")
-        return true
+        return c + 1
     }
     // Tie scenario
     else if ((humanSelection === "rock" && computerSelection === "rock") || (humanSelection ==="paper" && computerSelection === "paper") || (humanSelection === "scissors" && computerSelection === "scissors"
     )) {
-        console.log("User and Computer chose same optios, TIE!")
+        console.log("User and Computer both chose " + humanSelection + ", TIE!")
         return false
     }
 
 }
 
-// Call function, with variables of user and computer choice being used
+// Call function, with parameters of user and computer choice being used
 rps(humanSelection, computerSelection)
